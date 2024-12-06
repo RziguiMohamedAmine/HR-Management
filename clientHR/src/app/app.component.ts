11505f69a16fa89f899377c56128e8e30dba360c
RziguiMohamedAmine <62619234+RziguiMohamedAmine@users.noreply.github.com>
2024-12-06 10:44:52 +0100

    home

--- clientHR/src/app/app.component.ts
+++ clientHR/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { HomeComponent } from './layouts/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SidebarComponent],
+  imports: [RouterOutlet,HeaderComponent,SidebarComponent, HomeComponent],
   // encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent {  
   title = 'clientHR';
 }
