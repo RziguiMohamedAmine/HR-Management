c467fb730cbbdc8ab735d0e8e50184554ea0e3da
RziguiMohamedAmine <62619234+RziguiMohamedAmine@users.noreply.github.com>
2024-12-06 09:56:24 +0100

    Angular_Template_Integration

---
+++ clientHR/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,SidebarComponent],
+  // encapsulation: ViewEncapsulation.None,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'clientHR';
+}
